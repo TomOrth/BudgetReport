4bd23b0e2fc5e97185e6d8bc5c25b04db05a6da4
Tom Orth <torth212@gmail.com>
2018-08-04 22:55:29 -0400

    Standardize quotations

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ CSRF_ENABLED     = True
 
 # Use a secure, unique and absolutely secret key for
 # signing the data. 
-CSRF_SESSION_KEY = "somethingsecret"
+CSRF_SESSION_KEY = 'somethingsecret'
 
 SQLALCHEMY_DATABASE_URI = 'postgres://{}:{}@{}/budgetreport'.format(os.environ['RDS_USER'], os.environ['RDS_PASSWRD'], os.environ['RDS_HOST'])
 
-SECRET_KEY = "secret"
+SECRET_KEY = 'secret'
