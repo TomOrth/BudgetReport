b922af6de128ed00cc1842112bacf1819a6723dc
Tom <torth212@gmail.com>
2019-09-15 23:21:07 -0400

    Change database url

--- config.py
+++ config.py
@@ -2,16 +2,19 @@
 DEBUG = True
 
 import os
-#BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
+#BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Enable protection agains *Cross-site Request Forgery (CSRF)*
 CSRF_ENABLED     = True
 
 # Use a secure, unique and absolutely secret key for
-# signing the data. 
+# signing the data.
 CSRF_SESSION_KEY = 'somethingsecret'
 
-SQLALCHEMY_DATABASE_URI = 'postgres://{}:{}@{}/budgetreport'.format(os.environ['RDS_USER'], os.environ['RDS_PASSWRD'], os.environ['RDS_HOST'])
+SQLALCHEMY_DATABASE_URI = 'sqlite://database.db'
+
+# uncomment to use postgres and aws
+#SQLALCHEMY_DATABASE_URI = 'postgres://{}:{}@{}/budgetreport'.format(os.environ['RDS_USER'], os.environ['RDS_PASSWRD'], os.environ['RDS_HOST'])
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
